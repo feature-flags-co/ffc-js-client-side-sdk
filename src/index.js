e25795aea6ce3db7ab8b4de12c21e7af7e87fa27
cosmos-explorer <lian.yang.work@gmail.com>
2021-09-09 23:04:14 +0200

    use the new endpoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-
-
 var FFCJsClient = {
     user: {
         userName: '',
@@ -9,45 +7,43 @@ var FFCJsClient = {
         customizeProperties: []
     },
     environmentSecret: '',
-    initialize: function (environmentSecret, user) {
-        this.environmentSecret = environmentSecret;
-        this.user = user;
-    },
+    baseUrl: 'https://api.feature-flags.co',
+    initialize: function (environmentSecret, user, baseUrl) {
+		this.environmentSecret = environmentSecret;
+		this.user = user;
+		this.baseUrl = baseUrl || this.baseUrl;
+	},
 
-    variation: function (featureFlagKey, isHttps = true) {
-        const Http = new XMLHttpRequest();
-        const url = 'https://api.feature-flags.co';
-        if (isHttps === false || isHttps === 'false')
-            url = 'http://api.feature-flags.co';
-        var postUrl = url + '/Variation/GetUserVariationResult';
+	variation: function (featureFlagKey, defaultResult = false) {
+		try {
+			var postUrl = this.baseUrl + '/Variation/GetMultiOptionVariation';
 
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", postUrl, false);
+			var xhr = new XMLHttpRequest();
+			xhr.open("POST", postUrl, false);
 
-        xhr.setRequestHeader("Content-type", "application/json");
+			xhr.setRequestHeader("Content-type", "application/json");
 
-        var sendData = {
-            featureFlagKeyName: featureFlagKey,
-            environmentSecret: this.environmentSecret,
-            ffUserName: this.user.userName,
-            ffUserEmail: this.user.email,
-            ffUserCountry: this.user.country,
-            ffUserKeyId: this.user.key,
-            ffUserCustomizedProperties: this.user.customizeProperties
-        }
-        //将用户输入值序列化成字符串
-        xhr.send(JSON.stringify(sendData));
+			var sendData = {
+				featureFlagKeyName: featureFlagKey,
+				environmentSecret: this.environmentSecret,
+				ffUserName: this.user.userName,
+				ffUserEmail: this.user.email,
+				ffUserCountry: this.user.country,
+				ffUserKeyId: this.user.key,
+				ffUserCustomizedProperties: this.user.customizeProperties
+			}
+			//将用户输入值序列化成字符串
+			xhr.send(JSON.stringify(sendData));
 
-        if (xhr.status === 200) {
-            console.log(xhr.responseText);
-        }
-        if (xhr.responseText === 'true')
-            return true;
-        if (xhr.responseText === 'false')
-            return false;
+			if (xhr.status !== 200) {
+				return defaultResult;
+			}
 
-        return xhr.responseText;
-    }
+			return JSON.parse(xhr.responseText).variationValue;
+		} catch (err) {
+			return defaultResult;
+		}
+	}
 }
 
 export { FFCJsClient };
