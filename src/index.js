9f6d0d4539fdd9f5b9590fe4a20bd3b1ed88fe22
cosmos-explorer <lian.yang.work@gmail.com>
2021-09-14 23:06:08 +0200

    User proxy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-var FFCJsClient = {
-    user: {
+const kernal = {
+	user: {
         userName: '',
         email: '',
         country: '',
@@ -7,43 +7,99 @@ var FFCJsClient = {
         customizeProperties: []
     },
     environmentSecret: '',
+	storage: {setItem: () => {}, getItem: () => '{}'},
     baseUrl: 'https://api.feature-flags.co',
-    initialize: function (environmentSecret, user, baseUrl) {
+	initialize: function (environmentSecret, user, storage, baseUrl) {
 		this.environmentSecret = environmentSecret;
 		this.user = user;
 		this.baseUrl = baseUrl || this.baseUrl;
+		this.storage = storage || this.storage;
 	},
+	getPayloadStr(featureFlagKey) {
+		return JSON.stringify({
+			featureFlagKeyName: featureFlagKey,
+			environmentSecret: this.environmentSecret,
+			ffUserName: this.user.userName,
+			ffUserEmail: this.user.email,
+			ffUserCountry: this.user.country,
+			ffUserKeyId: this.user.key,
+			ffUserCustomizedProperties: this.user.customizeProperties
+		});
+	},
+	async variationAsync(featureFlagKey) {
+		const storageKey = `ffc_ffkey_${featureFlagKey}`;
+		try {
+			var postUrl = this.baseUrl + '/Variation/GetMultiOptionVariation';
 
-	variation: function (featureFlagKey, defaultResult = false) {
+			const response = await fetch(postUrl, {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				  },
+				  body: this.getPayloadStr(featureFlagKey)
+			});
+
+			if (!response.ok) {
+				throw new Error(`An error has occured: ${response.status}`);
+			}
+
+			const result = await response.text();
+			this.storage.setItem(storageKey, result);
+			return JSON.parse(result);
+		} catch(error) {
+			console.log(error);
+			var item = this.storage.getItem(storageKey);
+			if (item === null) {
+				return null;
+			}
+
+			return JSON.parse(item);
+		}
+	},
+	variationSync(featureFlagKey) {
+		const storageKey = `ffc_ffkey_${featureFlagKey}`;
 		try {
 			var postUrl = this.baseUrl + '/Variation/GetMultiOptionVariation';
 
 			var xhr = new XMLHttpRequest();
 			xhr.open("POST", postUrl, false);
-
 			xhr.setRequestHeader("Content-type", "application/json");
 
-			var sendData = {
-				featureFlagKeyName: featureFlagKey,
-				environmentSecret: this.environmentSecret,
-				ffUserName: this.user.userName,
-				ffUserEmail: this.user.email,
-				ffUserCountry: this.user.country,
-				ffUserKeyId: this.user.key,
-				ffUserCustomizedProperties: this.user.customizeProperties
-			}
 			//将用户输入值序列化成字符串
-			xhr.send(JSON.stringify(sendData));
+			xhr.send(this.getPayloadStr(featureFlagKey));
 
 			if (xhr.status !== 200) {
-				return defaultResult;
+				throw new Error(`An error has occured: ${xhr.status}`);
 			}
 
-			return JSON.parse(xhr.responseText).variationValue;
-		} catch (err) {
-			return defaultResult;
+			this.storage.setItem(storageKey, xhr.responseText);
+			return JSON.parse(xhr.responseText);
+		} catch (error) {
+			console.log(error);
+			var item = this.storage.getItem(storageKey);
+			if (item === null) {
+				return null;
+			}
+
+			return JSON.parse(item);
 		}
 	}
-}
+};
 
-export { FFCJsClient };
+const client = new Proxy(kernal, {
+	get (target, name, receiver) {
+		if (name in target) {
+			const value = target[name];
+			if (typeof value === 'function') {
+				return value.bind(target);
+			} else {
+				return value;
+			}
+		} else {
+			return target.variationSync(name);
+		}
+	}
+});
+
+export { client };
