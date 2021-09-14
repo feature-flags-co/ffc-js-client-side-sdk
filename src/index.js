9c6634585e5a9c03070d0799ba7dcadffeb4eede
cosmos-explorer <lian.yang.work@gmail.com>
2021-09-15 00:02:52 +0200

    added track function

--- src/index.js
+++ src/index.js
@@ -13,7 +13,38 @@ var FFCJsClient = {
 		this.user = user;
 		this.baseUrl = baseUrl || this.baseUrl;
 	},
+	track: function (data) {
+		try {
+			var postUrl = this.baseUrl + '/ExperimentsDataReceiver/PushData';
 
+			var xhr = new XMLHttpRequest();
+			xhr.open("POST", postUrl, false);
+
+			xhr.setRequestHeader("Content-type", "application/json");
+
+			const payload = Object.assign({}, {
+				secret: this.environmentSecret,
+				user: {
+					ffUserName: this.user.userName,
+					ffUserEmail: this.user.email,
+					ffUserCountry: this.user.country,
+					ffUserKeyId: this.user.key,
+					ffUserCustomizedProperties: this.user.customizeProperties
+				}
+			}, data);
+
+			//将用户输入值序列化成字符串
+			xhr.send(JSON.stringify([payload]));
+
+			if (xhr.status !== 200) {
+				return false;
+			}
+
+			return true;
+		} catch (err) {
+			return false;
+		}
+	},
 	variation: function (featureFlagKey, defaultResult = false) {
 		try {
 			var postUrl = this.baseUrl + '/Variation/GetMultiOptionVariation';
