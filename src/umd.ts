6b1492a33caef6204ada64563aa0607982b60228
cosmos-explorer <lian.yang.work@gmail.com>
2021-11-06 10:12:15 +0800

    do not initialize when data-ffc-client not available

--- src/umd.ts
+++ src/umd.ts
@@ -3,12 +3,6 @@
 import { FFCJsClient } from "./index";
 import { IFFCUser } from "./types";
 
-const script = document.querySelector('script[data-ffc-client]');
-const envSecret = script?.getAttribute('data-ffc-client')
-if (!script || !envSecret) {
-    console.log('data-ffc-client attribute should be set on the feature-flags.co sdk');
-}
-
 // generate default user info
 function ffcguid() {
     let ffcHomePageGuid = localStorage.getItem("ffc-guid");
@@ -25,24 +19,30 @@ function ffcguid() {
     }
 }
 
-let sessionId = ffcguid();
-var c_name = 'JSESSIONID';
-if (document.cookie.length > 0) {
-    let c_start = document.cookie.indexOf(c_name + "=")
-    if (c_start != -1) {
-        c_start = c_start + c_name.length + 1
-        let c_end = document.cookie.indexOf(";", c_start)
-        if (c_end == -1) c_end = document.cookie.length
-        sessionId = unescape(document.cookie.substring(c_start, c_end));
+const script = document.querySelector('script[data-ffc-client]');
+const envSecret = script?.getAttribute('data-ffc-client')
+if (!script || !envSecret) {
+    console.log('data-ffc-client attribute should be set on the feature-flags.co sdk');
+} else {
+    let sessionId = ffcguid();
+    var c_name = 'JSESSIONID';
+    if (document.cookie.length > 0) {
+        let c_start = document.cookie.indexOf(c_name + "=")
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1
+            let c_end = document.cookie.indexOf(";", c_start)
+            if (c_end == -1) c_end = document.cookie.length
+            sessionId = unescape(document.cookie.substring(c_start, c_end));
+        }
     }
-}
-var user: IFFCUser = {
-    userName: sessionId,
-    email: `${sessionId}@anonymous.com`,
-    key: sessionId,
-    customizeProperties: []
-};
+    var user: IFFCUser = {
+        userName: sessionId,
+        email: `${sessionId}@anonymous.com`,
+        key: sessionId,
+        customizeProperties: []
+    };
 
-FFCJsClient.initialize(envSecret as string, user);
+    FFCJsClient.initialize(envSecret as string, user);
+}
 
 export { FFCJsClient }
